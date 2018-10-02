c6c41fd548db4b9523ccad20fa1a764c13272868
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-02 19:20:22 +0530

    Added gutter to contact page

--- src/js/components/main/contactComponent.js
+++ src/js/components/main/contactComponent.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
 export default function ContactComponent(props) {
+    const lineNums = [];
+    for (let i = 0; i < 5; i++) {
+        lineNums.push(i + 1);
+    }
+
     return (
         <div id="contact">
-            <h1>Contact Me</h1>
-            <form onSubmit={(e) => e.preventDefault()}>
-                <input type="text" name="name" placeholder="Name" autocomplete="off" autoFocus />
-                <input type="email" name="email" placeholder="Email" autocomplete="off" />
-                <input type="text" name="message" placeholder="Message" autocomplete="off" />
-                <input type="submit" value="Send message!" />
-            </form>
+            <div id="gutter">
+                {lineNums.map(n => <div>{n}</div>)}
+            </div>
+            <div>
+                <h1>Contact Me</h1>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <input type="text" name="name" placeholder="Name" autocomplete="off" autoFocus />
+                    <input type="email" name="email" placeholder="Email" autocomplete="off" />
+                    <input type="text" name="message" placeholder="Message" autocomplete="off" />
+                    <input type="submit" value="Send message!" />
+                </form>
+            </div>
         </div>
     );
 }
