acf091d4d245b1942488e709188daf5f641483ef
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-02 19:24:14 +0530

    Added gutter to about page

--- src/js/components/main/aboutComponent.js
+++ src/js/components/main/aboutComponent.js
@@ -5,12 +5,24 @@ renderjson.set_icons('', '').set_max_string_length(80).set_show_to_level('all');
 
 class AboutComponent extends React.Component {
     componentDidMount() {
-        document.querySelector('main #about').appendChild(renderjson(this.props.data));
+        document.querySelector('#about-content').appendChild(renderjson(this.props.data));
     }
 
     render() {
+        const lineNums = [];
+        for (let i = 0; i < 23; i++) {
+            lineNums.push(i + 1);
+        }
+
         return (
-            <div id="about"></div>
+            <div id="about">
+                <div id="gutter">
+                    {lineNums.map(n => <div>{n}</div>)}
+                </div>
+                <div id="about-content">
+
+                </div>
+            </div>
         );
     }
 }
