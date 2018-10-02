8b6f4dd84e963ed394384035de091a4548c3194b
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-02 18:57:33 +0530

    Styled contact component

--- src/js/components/main/contactComponent.js
+++ src/js/components/main/contactComponent.js
@@ -4,11 +4,11 @@ export default function ContactComponent(props) {
     return (
         <div id="contact">
             <h1>Contact Me</h1>
-            <form>
-                <input type="text" name="name" placeholder="Name" />
-                <input type="email" name="email" placeholder="Email" />
-                <textarea name="message" placeholder="Message" ></textarea>
-                <input type="submit" />
+            <form onSubmit={(e) => e.preventDefault()}>
+                <input type="text" name="name" placeholder="Name" autocomplete="off" autoFocus />
+                <input type="email" name="email" placeholder="Email" autocomplete="off" />
+                <input type="text" name="message" placeholder="Message" autocomplete="off" />
+                <input type="submit" value="Send message!" />
             </form>
         </div>
     );
